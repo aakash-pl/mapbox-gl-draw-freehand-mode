4ab577c9065aa796463897d948e0cfbcb3b751e7
Aakash Yadav <aakash@Aakashs-MacBook-Pro.local>
2024-10-22 16:37:51 +0530

    fix: get coordinates on mouse up

--- src/index.js
+++ src/index.js
@@ -55,8 +55,19 @@ FreehandMode.onDrag = FreehandMode.onTouchMove = function (state, e) {
 };
 
 FreehandMode.onMouseUp = function (state, e) {
+  state.polygon.updateCoordinate(
+    `0.${state.currentVertexPosition}`,
+    e.lngLat.lng,
+    e.lngLat.lat
+  );
+  state.currentVertexPosition++;
+  state.polygon.updateCoordinate(
+    `0.${state.currentVertexPosition}`,
+    e.lngLat.lng,
+    e.lngLat.lat
+  );
   if (state.dragMoving) {
-    // this.simplify(state.polygon);
+    this.simplify(state.polygon);
     this.fireUpdate();
     this.changeMode(modes.SIMPLE_SELECT, { featureIds: [state.polygon.id] });
   }
